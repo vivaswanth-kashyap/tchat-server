90b0c711d77d9ce59e0ee174aa9ce6dbb25d74b7
Vivaswanth Kashyap <kashyap.vivaswanth@gmail.com>
2025-06-18 14:14:17 -0400

    Username and ID bug fix, send and read message

--- internal/handlers/message.go
+++ internal/handlers/message.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/vivaswanth-kashyap/tchat-server/internal/models"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -20,12 +21,50 @@ func NewMessageHandlers(db *gorm.DB) *MessageHandlers {
 	return &MessageHandlers{DB: db}
 }
 
+type MessageRequest struct {
+	SenderID         string `json:"sender_id"`
+	ReceiverID       string `json:"receiver_id,omitempty"`
+	ReceiverUsername string `json:"receiver_username,omitempty"`
+	Body             string `json:"body"`
+}
+
 func (h *MessageHandlers) NewMessage(c *gin.Context) {
-	var message models.Message
-	if err := c.ShouldBindJSON(&message); err != nil {
+	var req MessageRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid request format: %v", err.Error())})
+	}
+
+	receiverID := req.ReceiverID
+	if req.ReceiverUsername != "" {
+		var user models.User
+		if err := h.DB.Where("username = ?", req.ReceiverUsername).First(&user).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User Not Found"})
+			return
+		}
+		receiverID = user.ID.String()
+	}
+
+	if receiverID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "receiver_id or receiver_username required"})
 		return
 	}
+
+	senderUUID, err := uuid.Parse(req.SenderID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid sender_id format"})
+	}
+
+	receiverUUID, err := uuid.Parse(req.ReceiverID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invaliud receiver_id format"})
+	}
+
+	message := models.Message{
+		SenderID:   senderUUID,
+		ReceiverID: receiverUUID,
+		Body:       req.Body,
+	}
+
 	result := h.DB.Clauses(clause.Returning{}).Create(&message)
 	if result.Error != nil {
 		log.Printf("Error creating new messaging: %v", result.Error)
@@ -60,10 +99,15 @@ func (h *MessageHandlers) ReadMessage(c *gin.Context) {
 
 func (h *MessageHandlers) ReadChat(c *gin.Context) {
 	senderId := c.Query("sender_id")
-	receiverId := c.Query("receiver_id")
 
-	if senderId == "" || receiverId == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "senderid and/or receiverid missing"})
+	if senderId == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "senderid missing"})
+	}
+
+	receiverId, err := h.resolveReceiverID(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	var messages []models.Message
@@ -81,10 +125,15 @@ func (h *MessageHandlers) ReadChat(c *gin.Context) {
 
 func (h *MessageHandlers) ReadLastSent(c *gin.Context) {
 	senderId := c.Query("sender_id")
-	receiverId := c.Query("receiver_id")
 
-	if senderId == "" || receiverId == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "senderid and/or receiverid missing"})
+	if senderId == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "senderid missing"})
+	}
+
+	receiverId, err := h.resolveReceiverID(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	var message models.Message
@@ -104,4 +153,22 @@ func (h *MessageHandlers) ReadLastSent(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": message})
 
+}
+
+func (h *MessageHandlers) resolveReceiverID(c *gin.Context) (string, error) {
+	receiverId := c.Query("receiver_id")
+	receiverUsername := c.Query("receiver_username")
+
+	if receiverUsername == "" {
+		return "", fmt.Errorf("receiver id or username required")
+	}
+
+	if receiverUsername != "" {
+		var user models.User
+		if err := h.DB.Where("username = ?", receiverUsername).First(&user).Error; err != nil {
+			return "", fmt.Errorf("user not found")
+		}
+		return user.ID.String(), nil
+	}
+	return receiverId, nil
 }
